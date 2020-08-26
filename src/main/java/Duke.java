59440503c7e105d314ce1bc4f31b104fe1378b60
John <e0407302@u.nus.edu>
2020-08-26 13:39:09 +0800

    Implement level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String userInput;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,6 +13,16 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Duke\n" +
                 " What can I do for you?");
+        System.out.println("____________________________________________________________");
+        while(true){
+            userInput = in.nextLine();
+            if(userInput.equalsIgnoreCase("bye")){
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" "+userInput);
+            System.out.println("____________________________________________________________");
+        }
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
