52258fb6517bf22d6fd20b458c6316726ba24a4c
John <e0407302@u.nus.edu>
2020-08-26 17:58:05 +0800

    Implement Level-2 Add class Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        ArrayList<Task> taskList = new ArrayList<Task>();
         String userInput;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -14,14 +16,29 @@ public class Duke {
         System.out.println(" Hello! I'm Duke\n" +
                 " What can I do for you?");
         System.out.println("____________________________________________________________");
-        while(true){
+        while (true) {
             userInput = in.nextLine();
-            if(userInput.equalsIgnoreCase("bye")){
+            if (userInput.equalsIgnoreCase("bye")) {
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                if (taskList.size() == 0) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Oops the list is empty!");
+                    System.out.println("____________________________________________________________");
+                    continue;
+                }
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskList.size(); i++) {
+                    Task t = taskList.get(i);
+                    System.out.println(" " + (i + 1) + ". " + t.description);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                taskList.add(new Task(userInput));
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + userInput);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(" "+userInput);
-            System.out.println("____________________________________________________________");
         }
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+    public Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718");
+    }
+}
